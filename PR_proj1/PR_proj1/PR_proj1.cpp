043d6c98f0422788c0dccd66aa4d6599b3efd917
Jan <fokzterrier@gmail.com>
2019-04-08 14:35:17 +0200

    First attempt to parralel project

---
+++ PR_proj1/PR_proj1/PR_proj1.cpp
@@ -0,0 +1,202 @@
+﻿// PR_proj1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+
+
+// Celem tego programu jest prezentacja pomiaru i analizy 
+//efektywnosci programu za pomocš  CodeAnalyst(tm).
+// Implementacja mnożenia macierzy jest realizowana za pomoca typowego 
+// algorytmu podręcznikowego. 
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include "omp.h"
+
+#define USE_MULTIPLE_THREADS true
+#define MAXTHREADS 128
+int NumThreads;
+double start;
+
+static const int ROWS = 1000;     // liczba wierszy macierzy
+static const int COLUMNS = 1000;  // lizba kolumn macierzy
+
+float matrix_a[ROWS][COLUMNS];    // lewy operand 
+float matrix_b[ROWS][COLUMNS];    // prawy operand
+float matrix_r[ROWS][COLUMNS];    // wynik
+
+FILE *result_file;
+
+void initialize_matrices()
+{
+	// zdefiniowanie zawarosci poczatkowej macierzy
+//#pragma omp parallel for 
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLUMNS; j++) {
+			matrix_a[i][j] = (float)rand() / RAND_MAX;
+			matrix_b[i][j] = (float)rand() / RAND_MAX;
+			matrix_r[i][j] = 0.0;
+		}
+	}
+}
+
+void initialize_matricesZ()
+{
+	// zdefiniowanie zawarosci poczatkowej macierzy
+#pragma omp parallel for 
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLUMNS; j++) {
+			matrix_r[i][j] = 0.0;
+		}
+	}
+}
+void print_result()
+{
+	// wydruk wyniku
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLUMNS; j++) {
+			fprintf(result_file, "%6.4f ", matrix_r[i][j]);
+		}
+		fprintf(result_file, "\n");
+	}
+}
+
+void multiply_matrices_IJK_1()
+{
+	// mnozenie macierzy 
+	//pragma omp przed pierwszą pętlą
+#pragma omp parallel for 
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLUMNS; j++) {
+			float sum = 0.0;
+			for (int k = 0; k < COLUMNS; k++) {
+				sum = sum + matrix_a[i][k] * matrix_b[k][j];
+			}
+			matrix_r[i][j] = sum;
+		}
+	}
+}
+
+void multiply_matrices_IJK_2()
+{
+	// mnozenie macierzy 
+	// pragma omp przed drugą pętlą
+	for (int i = 0; i < ROWS; i++) {
+		#pragma omp parallel for 
+		for (int j = 0; j < COLUMNS; j++) {
+			float sum = 0.0;
+			for (int k = 0; k < COLUMNS; k++) {
+				sum = sum + matrix_a[i][k] * matrix_b[k][j];
+			}
+			matrix_r[i][j] = sum;
+		}
+	}
+}
+
+void multiply_matrices_IJK_3()
+{
+	// mnozenie macierzy  
+	// pragma omp przed trzecią pętlą
+	// może jebnąć
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLUMNS; j++) {
+			
+		#pragma omp parallel{
+			float sum = 0.0;
+			#pragma omp for	
+			for (int k = 0; k < COLUMNS; k++) {
+				sum = sum + matrix_a[i][k] * matrix_b[k][j];
+			}
+			#pragma omp atomic
+			matrix_r[i][j] += sum;
+			}
+		}
+	}
+
+
+	//walidacja 
+	//porównanie do IKJ żeby upewnić się czy wyniki są dobre
+	// floaty porównywać deltę do około 0.001 
+	//zapisywać do kopii macierzy
+	// na przykład Bool
+
+void multiply_matrices_IJK_S()
+{
+	// mnozenie macierzy  
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLUMNS; j++) {
+			float sum = 0.0;
+			for (int k = 0; k < COLUMNS; k++) {
+				sum = sum + matrix_a[i][k] * matrix_b[k][j];
+			}
+			matrix_r[i][j] = sum;
+		}
+	}
+}
+
+//dodać sekwencyjną IKJ
+
+
+
+
+void print_elapsed_time()
+{
+	double elapsed;
+	double resolution;
+
+	// wyznaczenie i zapisanie czasu przetwarzania
+	elapsed = (double)clock() / CLK_TCK;
+	resolution = 1.0 / CLK_TCK;
+	printf("Czas: %8.4f sec \n",
+		elapsed - start);
+
+	fprintf(result_file,
+		"Czas wykonania programu: %8.4f sec (%6.4f sec rozdzielczosc pomiaru)\n",
+		elapsed - start, resolution);
+}
+
+int main(int argc, char* argv[])
+{
+	//	 start = (double) clock() / CLK_TCK ;
+	if ((result_file = fopen("classic.txt", "a")) == NULL) {
+		fprintf(stderr, "nie mozna otworzyc pliku wyniku \n");
+		perror("classic");
+		return(EXIT_FAILURE);
+	}
+
+
+	//Determine the number of threads to use
+	if (USE_MULTIPLE_THREADS) {
+		SYSTEM_INFO SysInfo;
+		GetSystemInfo(&SysInfo);
+		NumThreads = SysInfo.dwNumberOfProcessors;
+		if (NumThreads > MAXTHREADS)
+			NumThreads = MAXTHREADS;
+	}
+	else
+		NumThreads = 1;
+	fprintf(result_file, "Klasyczny algorytm mnozenia macierzy, liczba watkow %d \n", NumThreads);
+	printf("liczba watkow  = %d\n\n", NumThreads);
+
+	initialize_matrices();
+	start = (double)clock() / CLK_TCK;
+	multiply_matrices_IJK_1();
+	printf("IJK ");
+	print_elapsed_time();
+	initialize_matricesZ();
+
+
+	fclose(result_file);
+
+	return(0);
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
